7a835194f9332078047c1586f7fc856ce1a5de15
Manuel Pagoada <mpagoada@gmail.com>
2025-08-21 21:00:14 -0700

    added template and background code

--- flaskdemo.py
+++ flaskdemo.py
@@ -1,12 +1,18 @@
 #!/bin/python3
 
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+
+    portugalTimeZone = pytz.timezone('Europe/Lisbon')
+    portugalTime = datetime.now(portugalTimeZone).strftime('%Y-%m-%d %H:%M:%S')
+
+    return render_template('index.html', time=portugalTime)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
